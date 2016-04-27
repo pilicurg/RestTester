a149c594fe602c99e0108f053090c0279c496074
lf <lf@lfdeMacBook-Pro.local>
2016-04-28 00:01:43 +0800

    able to obtain tc_list file

--- src/Parser.py
+++ src/Parser.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python
 # Parser.py
-# load json files and parse into Python data types
+# loads json files and parse into Python data types
 
 import os.path
 import json
 
+class Parser(object):
+    def __init__(self, list_file):
+        self._d = {}
+        with open(list_file) as f:
+            self._data = json.load(f)
+
+    def _tc_files(self):
+         return self._data.get(u'list',[])
+
+    def get_data(self):
+        return self._data
+
+    def encode(self):
+        for tc in self._tc_files():
+            tc_name = os.path.basename(tc)
+            self._d[tc_name] = {}
+
 
 if __name__ == '__main__':
-    project_dir = os.path.join(os.path.dirname(__file__),'..')
-    print project_dir
-    test_list = os.path.join(project_dir,"/TC/TC_list.json")
-    # with open(test_list) as l:
-    #     d = json.load(l)
-    # print d
+    
+    file_name = "TC/TC_list.json"
+    project_dir = os.path.normpath(os.path.join(os.path.dirname(__file__),'..'))
+    list_file = os.path.normpath(os.path.join(project_dir, file_name))
+
+    p = Parser(list_file)
+    p.encode()
--- src/RestTester.py
+++ src/RestTester.py
@@ -2,5 +2,26 @@
 # RestTester.py
 # a controller of this project
 
+import os
+from Parser import Parser
+
+
+def get_list_file():
+    file_name = "TC/TC_list.json"
+
+    project_dir = os.path.normpath(os.path.join(os.path.dirname(__file__),'..'))
+    list_file = os.path.normpath(os.path.join(project_dir, file_name))
+
+    return list_file
+
+def main():
+    list_file = get_list_file()
+
+    p = Parser(list_file)
+    p.encode()
+
+if __name__ == '__main__':
+    main()
+
 
 
